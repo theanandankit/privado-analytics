869ca10b24ae87bb28b8d3832bb6d2b29fb56b3c
“Hitesh <hitesh.bedre@privado.com>
2022-05-30 00:40:00 +0530

    added log statement

--- src/main/java/com/analytics/analytics/services/OrderService.java
+++ src/main/java/com/analytics/analytics/services/OrderService.java
@@ -3,6 +3,8 @@ package com.analytics.analytics.services;
 import com.analytics.analytics.dao.IOrderDAO;
 import com.analytics.analytics.entity.Order;
 import com.analytics.analytics.entity.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +12,10 @@ import java.util.List;
 
 @Service
 public class OrderService implements IOrderService {
-	
+
+	Logger logger = LoggerFactory.getLogger(OrderService.class);
+
+
 	@Autowired
 	private IOrderDAO dao;
 
@@ -26,6 +31,8 @@ public class OrderService implements IOrderService {
 	public Order createOrder(Order order) {
 		Product product = new Product();
 		product.setOrderId(order.getId());
+		System.out.println("Buyer Email: " + order.getBuyer().getEmail());
+		logger.debug("Buyer Email: " + order.getBuyer().getEmail());
 		product.setBuyerEmail(order.getBuyer().getEmail());
 
 		productService.saveProduct(product);
