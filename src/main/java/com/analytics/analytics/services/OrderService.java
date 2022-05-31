ecf8e18f5a8e2904e70f81a9e01153883515d485
“Hitesh <hitesh.bedre@privado.com>
2022-05-31 15:40:03 +0530

    moved JIraService

--- src/main/java/com/analytics/analytics/services/OrderService.java
+++ src/main/java/com/analytics/analytics/services/OrderService.java
@@ -1,14 +1,23 @@
 package com.analytics.analytics.services;
 
+import com.analytics.analytics.client.JiraClient;
 import com.analytics.analytics.dao.IOrderDAO;
 import com.analytics.analytics.entity.Order;
 import com.analytics.analytics.entity.Product;
+import com.atlassian.jira.rest.client.api.IssueRestClient;
+import com.atlassian.jira.rest.client.api.domain.Comment;
+import com.atlassian.jira.rest.client.api.domain.Issue;
+import com.atlassian.jira.rest.client.api.domain.input.IssueInput;
+import com.atlassian.jira.rest.client.api.domain.input.IssueInputBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 public class OrderService implements IOrderService {
@@ -23,7 +32,7 @@ public class OrderService implements IOrderService {
 	private ProductService productService;
 
 	@Autowired
-	private JiraService jiraService;
+	private JiraServiceImpl jiraService;
 
 	@Override
 	public List<Order> getOrders() {
@@ -32,10 +41,11 @@ public class OrderService implements IOrderService {
 
 	@Override
 	public Order createOrder(Order order) {
+		String buyerEmail = order.getBuyer().email;
 		try {
 			Product product = new Product();
 			product.setOrderId(order.getId());
-			String buyerEmail = order.getBuyer().email;
+
 			System.out.println("Buyer Email: " + buyerEmail);
 			logger.debug("Buyer Email: " + buyerEmail);
 			product.setBuyerEmail(buyerEmail);
@@ -46,7 +56,7 @@ public class OrderService implements IOrderService {
 			logger.error("Exception while creating order: " +
 					""+ order.getId() + "" +
 					"Buyer Email"+ order.getBuyer().email);
-			String issueId = jiraService.createIssue(1l, "Exception while creating order for "+ order.getBuyer().email);
+			jiraService.createIssue(1l, "Exception while creating order for "+ buyerEmail);
 			return null;
 		}
 		return dao.createOrder(order);
@@ -65,6 +75,50 @@ public class OrderService implements IOrderService {
 	@Override
 	public boolean deleteOrder(int orderId) {
 		return dao.deleteOrder(orderId);
+	}
+
+}
+
+@Service
+class JiraServiceImpl {
+
+	@Value("${jira.username}")
+	private String username;
+
+	@Value("${jira.password}")
+	private String password;
+
+	@Value("${jira.url}")
+	private String jiraUrl;
+
+	@Value("${jira.project.key}")
+	private String projectKey;
+
+
+	private JiraClient jiraClient = new JiraClient(username, password, jiraUrl);
+	private IssueRestClient issueClient = jiraClient.getRestClient().getIssueClient();
+
+	public String createIssue(Long issueType, String email) {
+		String issueSummary = "Payment Failure by" + email;
+		IssueInput newIssue = new IssueInputBuilder(
+				projectKey, issueType, issueSummary).build();
+		return issueClient.createIssue(newIssue).claim().getKey();
+	}
+
+	public Issue getIssue(String issueKey) {
+		return issueClient
+				.getIssue(issueKey)
+				.claim();
+	}
+
+	public void addComment(Issue issue, String commentBody) {
+		jiraClient.getRestClient().getIssueClient()
+				.addComment(issue.getCommentsUri(), Comment.valueOf(commentBody));
+	}
+
+	public List<Comment> getAllComments(String issueKey) {
+		return StreamSupport.stream(getIssue(issueKey).getComments().spliterator(), false)
+				.collect(Collectors.toList());
 	}
 
 }
